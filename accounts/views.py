fba7cdaa1c3543f2e2da456b50c32584910dac98
Sunjoo IM <tjss1580@gmail.com>
2022-10-14 15:57:48 +0900

    accounts/login

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def index(request):
@@ -26,3 +28,20 @@ def signup(request):
     }
     return render(request, 'accounts/signup.html', context)
     
+
+def login(request):
+    if request.method == 'POST':
+        # AuthenticationForm은 ModelForm이 아님
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # 세션에 저장
+            # login 함수는 request, user 객체를 인자로 받음
+            # user 객체는 form에서 인증된 유저 정보
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
