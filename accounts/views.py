3b7f3e20a808c9d9a2921d2580e5b462c1eb60c3
Sunjoo IM <tjss1580@gmail.com>
2022-10-14 18:05:14 +0900

    logout

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model, update_session_auth_hash
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -85,3 +86,7 @@ def change_password(request):
     }
     return render(request, 'accounts/change_password.html', context)
     
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
