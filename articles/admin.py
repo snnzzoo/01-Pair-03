d58441bddc7e4135f8bd908bd4f0d037625f704e
Sunjoo IM <tjss1580@gmail.com>
2022-10-18 01:51:10 +0900

    Admin site

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie_name', 'created_at', 'updated_at')
+
+
+admin.site.register(Article, ArticleAdmin)
